653de48f3e778997a8f97e14c256f9225c9c86c2
unknown <justnotafox@gmail.com>
2018-11-18 18:57:11 -0500

    Added python scripts to replace C objects for ease of use with tabu sort

---
+++ ga.py
@@ -0,0 +1,10 @@
+import fitness2
+pop = []
+for x in range(0,20):
+	deck = fitness2.generateDeck()
+	fit = fitness2.fitness(deck,fitness2.initGamestate())
+	print("Deck",str(x+1),"fitness",str(fit))
+	pop.append((deck,fit))
+pop = sorted(pop,key=lambda x:x[1],reverse=1)
+print(pop)
+input()
