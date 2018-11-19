a1d1be3f337d4889ac19a0b7a7df901cca8cc1d0
unknown <justnotafox@gmail.com>
2018-11-18 22:43:46 -0500

    moved multithreading to ga code and assigned worker threads and a queue to improve overall performance

--- ga.py
+++ ga.py
@@ -1,11 +1,46 @@
 import fitness
+import random
+import threading
+import queue
+tocalc = queue.Queue()
 pop = []
+def worker():
+	while 1:
+		deck = tocalc.get()
+		fit = fitness.fitness(deck,fitness.initGamestate())
+		print(deck)
+		print(str(fit))
+		pop.append((deck,fit))
+		tocalc.task_done()
+
+for i in range(0,8):
+	threading.Thread(target=worker).start()
+def breed(mom,dad):
+	ret = {}
+	for i in fitness.vals:
+		if(bool(random.getrandbits(1))):
+			ret[i] = mom[i]
+		else:
+			ret[i] = dad[i]
+	c = sum(ret.values())
+	while(c > fitness.decksize):
+		t = random.choice(fitness.vals)
+		if(ret[t]):
+			ret[t] -= 1
+			c -= 1
+	while(c < fitness.decksize):
+		t = random.choice(fitness.vals)
+		if ret[t] < fitness.cardmax:
+			ret[t] += 1
+			c += 1
+	return ret
+	
 for x in range(0,20):
 	deck = fitness.generateDeck()
-	print(str(deck))
-	fit = fitness.fitness(deck,fitness.initGamestate())
-	print("Deck",str(x+1),"fitness",str(fit))
-	pop.append((deck,fit))
+	tocalc.put(deck)
+tocalc.join()
 pop = sorted(pop,key=lambda x:x[1],reverse=1)
-print(pop)
+deck = breed(pop[0][0],pop[1][0])
+fit = fitness.fitness(deck,fitness.initGamestate())
+print(str(fit))
 input()
