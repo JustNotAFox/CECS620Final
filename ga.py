43e598d12b65a0f4af3a2cc1a2fc6eb48031afc1
unknown <justnotafox@gmail.com>
2018-11-18 19:20:36 -0500

    further bugfixes in ga.py

--- ga.py
+++ ga.py
@@ -1,8 +1,8 @@
 import fitness
 pop = []
 for x in range(0,20):
-	deck = fitness2.generateDeck()
-	fit = fitness2.fitness(deck,fitness2.initGamestate())
+	deck = fitness.generateDeck()
+	fit = fitness.fitness(deck,fitness.initGamestate())
 	print("Deck",str(x+1),"fitness",str(fit))
 	pop.append((deck,fit))
 pop = sorted(pop,key=lambda x:x[1],reverse=1)
